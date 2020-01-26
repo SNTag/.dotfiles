4049008950bcca8ffeaa6c39277b323287511cee
SNTag <agenttiny@gmail.com>
2020-01-26 18:25:11 +0800

    theme loading modifications

--- emacs-26.2/Borg-Collective-Emacs/TagOS-themes.el
+++ emacs-26.2/Borg-Collective-Emacs/TagOS-themes.el
@@ -4,33 +4,19 @@
 ;; The theme is being set here.
 
 ;; ====================
-;; Ensuring themes and relevant packages are available
+;; Ensuring themes, icons are available
 
-;; (use-package leuven-theme)
-;; (use-package doom-themes)
+(use-package leuven-theme)
+(use-package doom-themes)
 (use-package all-the-icons)
 
 
 ;; ====================
-;; mode-line customization
-
-;; (use-package doom-modeline
-;;       :ensure t
-;;       :hook (after-init . doom-modeline-mode))
-
-;; ====================
 ;; Setting themes
+;;
+;; This enables a theme depending on running from terminal or GUI.
 
-;; (if (display-graphic-p)
-;;     (load-theme 'leuven-dark)
-;;     (load-theme 'doom-molokai)
-;;     )
-
-(add-to-list 'custom-theme-load-path "~/leuven-dark")
-(load-theme 'leuven t)                  ; For Emacs 24+.
-
-
-;; ====================
-;; theme customization
-
-;; (add-hook 'text-mode-hook 'turn-on-auto-fill)
+(if (display-graphic-p)
+    (load-theme 'leuven)
+    (load-theme 'doom-molokai)
+    )
