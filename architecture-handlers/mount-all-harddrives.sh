5cce0893ee59055a2c38689da295c492a0481978
SNTag <SNTagore@protonmail.com>
2020-06-24 16:55:32 +0800

    auto mounting harddrives is functional

--- architecture-handlers/mount-all-harddrives.sh
+++ architecture-handlers/mount-all-harddrives.sh
@@ -1,16 +1,15 @@
 ### for mounting all harddrives to ~/MyDiskX
-## WORK IN PROGRESS!!! DO NOT USE, HAS NOT BEEN TESTED WELL
 ## THIS IS NOT AN AUTOMATIC SCRIPT!!! MANUAL INTERVENTION IN THE CODE REQUIRED
-## will mount each partition under a drive to ~/MyDiskX
-## replace sdX* in the first line of code below with the drive number of interested.
+## This will mount each partition under a drive to ~/MyDiskX
+## Replace the 'drivenum' value (first line of code below) with the drive number of interested.
 ## For example, I want to mount all partitions under sda. replace sdX* with sda*
 
 # improvements to make:
 # - detect if drive already added in /etc/enviroment and /etc/fstab
-# - make first partition i="" instead of i="0" or i="1"
 # - detect partition file type for $filetype
+# - add to fstab by UUID for each partition.
 
-drivenum=sda
+drivenum=sdb
 username=sntag
 finalpath=MyDisk
 
@@ -22,8 +21,8 @@ do
     devdrivepath="/dev/$drivenum$i"
     finaldrivepath="/home/$username/$finalpath$i/"
     filetype="ext4"
-    sudo echo "$devdrivepath    $drivepath    $filetype    defaults    0    0" >> /etc/fstab
-    sudo echo "MYDISK$i='$drivepath'">> /etc/enviroment
+    echo "$devdrivepath    $finaldrivepath    $filetype    defaults    0    0" | sudo tee -a /etc/fstab
+    echo "MYDISK$i='$finaldrivepath'" | sudo tee -a /etc/environment
 
     i=$((i+1))
 done < ~/UUID-list.txt
